beae4bd652054ac0e92745d9ad8431ff0ced8e6e
pHoeniX-svg <vansomecsam@gmail.com>
2022-03-13 23:46:55 +0100

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,26 @@
+import Logo from '~src/images/logo.svg';
+import { ImgWrapper, Nav, Wrapper } from './style';
+
 export const Header = () => {
   return (
-    <header>
-      <ul role='list'>
-        <li>
-          <a href='#'>Features</a>
-        </li>
-        <li>
-          <a href='#'>Team</a>
-        </li>
-        <li>
-          <a href='#'>Sign In</a>
-        </li>
-      </ul>
-    </header>
+    <Wrapper>
+      <Nav>
+        <ImgWrapper>
+          <img src={Logo} alt='fylo logo' />
+        </ImgWrapper>
+
+        <ul role='list'>
+          <li>
+            <a href='#'>Features</a>
+          </li>
+          <li>
+            <a href='#'>Team</a>
+          </li>
+          <li>
+            <a href='#'>Sign In</a>
+          </li>
+        </ul>
+      </Nav>
+    </Wrapper>
   );
 };
